777fa561e05acf0ca100d344e6b92b3f6d7a58e3
Maye Khaled <mai.hussein00@eng-st.cu.edu.eg>
2022-12-08 01:21:09 +0200

    Front end init

--- voice_password/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-from voice_password import routes, mainRoute
--- voice_password/mainRoute.py
+++
@@ -1,6 +0,0 @@
-from voice_password import app 
-from flask import render_template
-
-@app.route('/')
-def main_route():
-    return render_template('index.html')
